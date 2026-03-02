c2a0592784c428e066afd5eb94b23629ef925591
Mark Vanbuyten <markvanbuyten@gmail.com>
2026-03-02 10:04:45 +0100

    2: setting up connection to Gemini API

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
 def main():
-    print("Hello from toyversionaiagent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise Exception("Gemini API Key was not found")
+
+    client = genai.Client(api_key=api_key)
+
+    model = "gemini-2.5-flash"
+    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+    response = client.models.generate_content(model = model, contents = contents)
+
+    print(f"{response.text}")
 
 
 if __name__ == "__main__":
