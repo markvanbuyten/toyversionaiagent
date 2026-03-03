7db6ec02451a49342b25a0b00549714ec242cc1f
Mark Vanbuyten <markvanbuyten@gmail.com>
2026-03-03 11:13:04 +0100

    8: added get_file_content and its testunit

---
+++ functions/get_file_content.py
@@ -0,0 +1,25 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    try:
+        working_directory_abs = os.path.abspath(working_directory)
+        target_file_path = os.path.normpath(os.path.join(working_directory_abs, file_path))
+
+        valid_target_dir = os.path.commonpath([working_directory_abs, target_file_path]) == working_directory_abs
+
+        if not valid_target_dir:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+        if not os.path.isfile(target_file_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+        with open(target_file_path, 'r', encoding='utf-8') as f:
+            content = f.read(MAX_CHARS)
+            if f.read(1):
+                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        return content
+    except Exception as e:
+        return f"Error: {e}"
