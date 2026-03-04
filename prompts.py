5a78cf92ee9740d23f0f02cf2e3d881ffbd65458
Mark Vanbuyten <markvanbuyten@gmail.com>
2026-03-04 07:51:30 +0100

    14: changed my prompt a little and tested with a self inflicted bug

--- prompts.py
+++ prompts.py
@@ -5,7 +5,7 @@ When a user asks a question or makes a request, make a function call plan. You c
 
 - List files and directories
 - Read file contents
-- Execute Python files with optional arguments
+- Execute Python files with optional arguments and if there are any bugs, fix the code
 - Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
